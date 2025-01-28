659da07af2eee3013c0d6cd9b0286fbb0a9eea05
Alex Kharichev <myx@myx.nz>
2025-01-28 23:53:59 +0200

    * HostApiHelper

--- java/ru/myx/ae3/internal/HostApiHelper.java
+++ java/ru/myx/ae3/internal/HostApiHelper.java
@@ -1,8 +1,10 @@
 package ru.myx.ae3.internal;
 
 import java.lang.management.ManagementFactory;
+import java.lang.management.MemoryMXBean;
 import java.lang.management.MemoryUsage;
 import java.lang.management.OperatingSystemMXBean;
+import java.lang.management.RuntimeMXBean;
 
 import ru.myx.ae3.Engine;
 
@@ -11,17 +13,21 @@ import ru.myx.ae3.Engine;
  */
 public class HostApiHelper {
 
+	private static final RuntimeMXBean RUNTIME_MX_BEAN = ManagementFactory.getRuntimeMXBean();
+	private static final MemoryMXBean MEMORY_MX_BEAN = ManagementFactory.getMemoryMXBean();
+	private static final OperatingSystemMXBean OPERATING_SYSTEM_MX_BEAN = ManagementFactory.getOperatingSystemMXBean();
+
 	/** @return */
 	public static double assessHostCpuLoad() {
 
-		final OperatingSystemMXBean osBean = ManagementFactory.getOperatingSystemMXBean();
+		final OperatingSystemMXBean osBean = HostApiHelper.OPERATING_SYSTEM_MX_BEAN;
 		return Math.min(1.0, 1.0 * osBean.getSystemLoadAverage() / osBean.getAvailableProcessors());
 	}
 	
 	/** @return */
 	public static double assessVmMemoryUsage() {
 
-		final MemoryUsage heapMemoryUsage = ManagementFactory.getMemoryMXBean().getHeapMemoryUsage();
+		final MemoryUsage heapMemoryUsage = HostApiHelper.MEMORY_MX_BEAN.getHeapMemoryUsage();
 		return Math.min(1.0, 1.0 * heapMemoryUsage.getUsed() / Math.max(heapMemoryUsage.getCommitted(), heapMemoryUsage.getMax()));
 	}
 	
@@ -40,6 +46,6 @@ public class HostApiHelper {
 	/** @return */
 	public static long getUptime() {
 
-		return ManagementFactory.getRuntimeMXBean().getUptime();
+		return HostApiHelper.RUNTIME_MX_BEAN.getUptime();
 	}
 }
