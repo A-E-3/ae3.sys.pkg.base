2974670d52733021c89403db8bc4ffeff4763427
Alex Kharichev <myx@myx.nz>
2025-07-02 20:59:17 +0200

    * minor: source code and commonjs type support in package.json

--- java/ru/myx/ae3/util/fn/SupplierVfsFolderMapCached.java
+++ java/ru/myx/ae3/util/fn/SupplierVfsFolderMapCached.java
@@ -11,36 +11,39 @@ import ru.myx.ae3.vfs.TreeReadType;
 
 /** @author myx */
 public abstract class SupplierVfsFolderMapCached extends SupplierMapAbstractCached {
-
+	
 	/**
 	 *
 	 */
 	protected final Entry folder;
-
+	
 	String previousCheck = null;
-
+	
 	/** @param folder */
 	public SupplierVfsFolderMapCached(final Entry folder) {
-
+		
 		this.folder = folder;
 	}
-
+	
 	@Override
 	public BaseObject checkReload(final BaseObject unchanged) {
-
+		
 		final Entry folder = this.folder;
 		if (!folder.isContainer()) {
 			return BaseObject.createObject();
 		}
-
+		
 		final BaseMapEditable result = BaseObject.createObject();
 		{
 			Map<String, Entry> folders = null;
 			Map<String, Entry> files = null;
-
+			
 			final StringBuilder check = new StringBuilder();
 			final BaseList<Entry> contents = folder.toContainer().getContentCollection(TreeReadType.ITERABLE).baseValue();
 			for (final Entry entry : contents) {
+				if (entry.getKey().startsWith(".")) {
+					continue;
+				}
 				if (entry.isContainer()) {
 					final String accepted = this.runFolderFilter(entry);
 					if (accepted != null) {
@@ -66,17 +69,17 @@ public abstract class SupplierVfsFolderMapCached extends SupplierMapAbstractCach
 					continue;
 				}
 			}
-
+			
 			if (unchanged != null && this.previousCheck != null && this.previousCheck.contentEquals(check)) {
 				return unchanged;
 			}
-
+			
 			if (folders != null) {
 				for (final Map.Entry<String, Entry> entry : folders.entrySet()) {
 					this.runFolderMapper(entry.getKey(), entry.getValue());
 				}
 			}
-
+			
 			if (files != null) {
 				for (final Map.Entry<String, Entry> entry : files.entrySet()) {
 					final String name = entry.getKey();
@@ -84,39 +87,39 @@ public abstract class SupplierVfsFolderMapCached extends SupplierMapAbstractCach
 					if (descriptor == null || descriptor == BaseObject.UNDEFINED || descriptor == BaseObject.NULL) {
 						continue;
 					}
-					
+
 					final BaseObject reduceResult = this.runDescriptorReducer(result, descriptor, name);
 					if (reduceResult != result) {
 						throw new IllegalStateException("Map is replaced by reducer callback, this: " + this);
 					}
 				}
 			}
-
+			
 			this.previousCheck = check.toString();
 		}
-
+		
 		return result;
 	}
-
+	
 	/** @param name
 	 * @return NULL when not accepted or key. */
 	protected abstract String runDescriptorFilter(final String name);
-
+	
 	/** @param key
 	 * @param entry
 	 * @return NULL when not accepted */
 	protected abstract BaseObject runDescriptorMapper(final Entry entry, final String key);
-
+	
 	/** @param result
 	 * @param descriptior
 	 * @param name
 	 * @return must return same result object */
 	protected abstract BaseObject runDescriptorReducer(final BaseMapEditable result, final BaseObject descriptior, final String name);
-
+	
 	/** @param entry
 	 * @return NULL when not accepted */
 	protected abstract String runFolderFilter(final Entry entry);
-	
+
 	/** @param key
 	 * @param entry
 	 * @return NULL when not accepted */
